00af86ccf105ff3125261d1ab3ec9d3e5b2f6f6f
Danny Waite <dannywaite7@gmail.com>
2016-10-10 21:06:06 +0100

    add gce detection script

---
+++ gce.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+KUBE_ENV=$(curl --fail --silent http://metadata.google.internal/computeMetadata/v1/instance/attributes/kube-env -H "Metadata-Flavor: Google")
+
+function parse_subnet {
+    local full_net=$1
+    local cidr=`echo ${full_net} | cut -d'/' -f2`
+    cidr=`convert_cidr ${cidr}`
+    echo "${cidr}"
+    }
+
+function convert_cidr {
+    local i netmask=""
+    local cidr=$1
+    local abs=$(($cidr/8))
+    for ((i=0;i<4;i+=1)); do
+    if [ $i -lt $abs ]; then
+    netmask+="255"
+    elif [ $i -eq $abs ]; then
+    netmask+=$((256 - 2**(8-$(($cidr%8)))))
+    else
+    netmask+=0
+    fi
+    test $i -lt 3 && netmask+="."
+    done
+    echo $netmask
+    }
+
+SERVICE_CLUSTER_IP_RANGE=$(echo "${KUBE_ENV}" | shyaml get-value "SERVICE_CLUSTER_IP_RANGE")
+CLUSTER_IP_RANGE=$(echo "${KUBE_ENV}" | shyaml get-value "CLUSTER_IP_RANGE")
+DNS_SERVER_IP=$(echo "${KUBE_ENV}" | shyaml get-value "DNS_SERVER_IP")
+
+SERVICE_SUBNET=$(parse_subnet ${SERVICE_CLUSTER_IP_RANGE})
+SERVICE_NETWORK=`echo ${SERVICE_CLUSTER_IP_RANGE} | cut -d'/' -f1`
+
+CLUSTER_SUBNET=$(parse_subnet ${CLUSTER_IP_RANGE})
+CLUSTER_NETWORK=`echo ${CLUSTER_IP_RANGE} | cut -d'/' -f1`
